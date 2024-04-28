1a34f59bc7a268fa76013ff3e7ccb954a975c0c2
danojeda <daniel.ojeda.escobar@gmail.com>
2024-04-28 01:30:13 -0400

    This is Prep4 for week 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers  =new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int element;
+        int suma=0;
+        int maximus=0;
+
+        float average;
+        do{
+            element = int.Parse(Console.ReadLine());
+            if (element !=0){
+                numbers.Add(element);
+                //Console.WriteLine($"{element}");
+            }
+            
+
+        }while (element !=0);
+
+
+        foreach (int number in numbers){
+            suma += number;
+            //Console.WriteLine($"{suma} >>> {number}");
+        }
+        Console.WriteLine($"The sum is: {suma}");
+
+        average = ((float)suma) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        maximus = numbers.Max();
+        Console.WriteLine($"The largest numner is : {maximus}");
+
     }
+
 }
