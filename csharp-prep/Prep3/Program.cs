f89031e2b7a76a104031280e30b17e6e8b4910c7
danojeda <daniel.ojeda.escobar@gmail.com>
2024-04-28 00:46:42 -0400

    This is prep3 for week 1 magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
-
+// Random randomGenerator = new Random();
+//int number = randomGenerator.Next(1, 11);
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       
+        int guess_number;
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next (1,100);
+
+        do 
+        {   
+            
+
+            Console.WriteLine("What is your guess?:");
+            guess_number = int.Parse(Console.ReadLine());
+
+            if (guess_number > magic_number){
+                Console.WriteLine("Lower");
+            }else if (guess_number < magic_number){
+                Console.WriteLine("Higher");
+            }else{
+                Console.WriteLine("You guessed it!");
+            }
+
+
+        }while (guess_number != magic_number);
+        
+
+
     }
 }
