770e37eaf806b92223586706d69830f338d95282
danojeda <daniel.ojeda.escobar@gmail.com>
2024-04-28 00:27:01 -0400

    Prep 2 from week 2 grade letter

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please entrer your grade percentage:");
+        int grade_percent= int.Parse(Console.ReadLine());
+        string grade_letter;
+
+        if (grade_percent >=90){
+            grade_letter = "A";
+            
+        }else if (grade_percent >= 80){
+            grade_letter = "B";
+            
+        }else if (grade_percent >= 70){
+            grade_letter="C";
+            
+        }else if(grade_percent >= 60)
+        {
+            grade_letter = "D";
+        }else{
+            grade_letter = "F";
+        }
+
+
+        Console.WriteLine($"Your letter grade is: {grade_letter}.");
+
+
+        if (grade_percent >= 70)
+        {
+            Console.WriteLine("Congratulations!, you have passed the course!.");
+
+        } else{
+            Console.WriteLine("Best luck next time.");
+        }
     }
 }
